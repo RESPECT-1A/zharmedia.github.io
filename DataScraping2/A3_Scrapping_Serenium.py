013469938613267b3a048ec10c5b526b356b0bfb
Raxoria <76510278+Raxoria@users.noreply.github.com>
2021-04-27 18:42:27 +0700

    update yey

--- DataScraping2/A3_Scrapping_Serenium.py
+++ DataScraping2/A3_Scrapping_Serenium.py
@@ -2,45 +2,35 @@ from selenium import webdriver
 import urllib.request
 from selenium.common.exceptions import NoSuchElementException
 import json
-import datetime
+from datetime import datetime
 
-PATH = "D:\\02 ZHAR'S PROJECT\\JTK POLBAN\\SEMESTER 2\\Proyek 1\\Tugas 7\\chromedriver_win32\\chromedriver.exe"
+PATH = "D:\Kuliah\Semester 2\Project 1\Web Selenium\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-driver.get(
-    "https://screenagewasteland.com/the-100-greatest-cartoons-of-all-time-10-1/")
+driver.get("https://www.billboard.com/charts/artist-100")
 
-animelist = []
+artistlist = []
 i = 1
 
-while i <= 100:
-    for artist in driver.find_elements_by_class_name("entry-content"):
-        driver.execute_script(
-            "arguments[0].scrollIntoView();", artist)  # auto scroll
-        print(artist.text)
-        for img in artist.find_elements_by_class_name("aligncenter"):
-            try:
-                url = img.get_attribute("src")
-                urllib.request.urlretrieve(url, str(i)+".png")
-                print(url)
-            except:
-                continue
-            i = i+1
-            animelist.append(
+now = datetime.now()
+time = now.strftime("%d %B %Y %H:%M:%S")
+
+for artist in driver.find_elements_by_class_name("chart-list-item"):
+    print(artist.text)
+    print(artist.find_element_by_class_name("chart-list-item__image").get_attribute("src"))
+    # urllib.request.urlretrieve(artist.find_element_by_class_name("chart-list-item__image").get_attribute("src"), str(i)+".png")
+    artistlist.append(
                 {
-                    "Keterangan": artist.text.split("\n")[3],
-                    "Judul": artist.text.split("\n")[1],
-                    "Rating": artist.text.split("\n")[5].split()[0],
-                    "Image": img.get_attribute("src"),
-                    "waktuscrapping": datetime.datetime.now().strftime("%d %B %Y %H:%M:%S")
+                    "Nomer": artist.text.split('\n')[0],
+                    "Name": artist.text.split('\n')[1],
+                    "Last": artist.text.split('\n')[2].split(' ')[0],
+                    "Peak": artist.text.split('\n')[2].split(' ')[3],
+                    "Weeks": artist.text.split('\n')[2].split(' ')[6],
+                    "TimeToScrapping": time,
+                    "Profile": artist.find_element_by_class_name("chart-list-item__image").get_attribute("src")
                 }
             )
-    try:
-        driver.find_element_by_class_name(
-            "pagination").find_element_by_partial_link_text("Next").click()
-    except NoSuchElementException as e:
-        break
 
 hasil_scrapping = open("hasilscraping.json", "w")
-json.dump(animelist, hasil_scrapping, indent=6)
+json.dump(artistlist, hasil_scrapping, indent=6)
 hasil_scrapping.close()
 driver.quit()
