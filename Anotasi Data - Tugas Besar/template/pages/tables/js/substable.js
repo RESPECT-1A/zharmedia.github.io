b1dd6f4a74d7efe36db2c68ba8ddadf61d930f54
M Azhar Alauddin <muhammadazharalauddin@gmail.com>
2021-06-24 23:29:03 +0700

    Update Data Table

--- Anotasi Data - Tugas Besar/template/pages/tables/js/substable.js
+++ Anotasi Data - Tugas Besar/template/pages/tables/js/substable.js
@@ -1,5 +1,6 @@
+// 1
 $(document).ready(function () {
-  $('#subscribers-table').DataTable({
+  $('#subscribers-table1').DataTable({
     dom: 'Bfrtip',
     buttons: [
       {
@@ -15,8 +16,333 @@ $(document).ready(function () {
   });
 });
 
+// 2
 $(document).ready(function () {
-  $('#subscribers-table1').DataTable({
+  $('#subscribers-table2').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 3
+$(document).ready(function () {
+  $('#subscribers-table3').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 4
+$(document).ready(function () {
+  $('#subscribers-table4').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 5
+$(document).ready(function () {
+  $('#subscribers-table5').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 6
+$(document).ready(function () {
+  $('#subscribers-table6').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 7
+$(document).ready(function () {
+  $('#subscribers-table7').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 8
+$(document).ready(function () {
+  $('#subscribers-table8').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 9
+$(document).ready(function () {
+  $('#subscribers-table9').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 10
+$(document).ready(function () {
+  $('#subscribers-table10').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 11
+$(document).ready(function () {
+  $('#subscribers-table11').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 12
+$(document).ready(function () {
+  $('#subscribers-table12').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 13
+$(document).ready(function () {
+  $('#subscribers-table13').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 14
+$(document).ready(function () {
+  $('#subscribers-table14').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 15
+$(document).ready(function () {
+  $('#subscribers-table15').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 16
+$(document).ready(function () {
+  $('#subscribers-table16').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 17
+$(document).ready(function () {
+  $('#subscribers-table17').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 18
+$(document).ready(function () {
+  $('#subscribers-table18').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 19
+$(document).ready(function () {
+  $('#subscribers-table19').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      {
+        extend: 'csvHtml5',
+        text: '<i class="mdi mdi-printer"></i> CSV',
+        titleAttr: 'CSV',
+        title: 'Subscribers',
+        exportOptions: {
+          columns: ':not(:last-child)',
+        },
+      },
+    ],
+  });
+});
+
+// 20
+$(document).ready(function () {
+  $('#subscribers-table20').DataTable({
     dom: 'Bfrtip',
     buttons: [
       {
